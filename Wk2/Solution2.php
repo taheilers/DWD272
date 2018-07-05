142b319257f1cb2d0e01b648a733cfa099edb1a2
Tim Heilers <theile1083@my.sctd.edu>
2018-07-05 18:53:13 -0400

    Changed variables to bold.

--- Wk2/Solution2.php
+++ Wk2/Solution2.php
@@ -1,4 +1,4 @@
 <?php 
     $color = array('white', 'green', 'red', 'blue', 'black', 'leaden');
-    echo('<p>"The memory of that scene for me is like a frame of film forever frozen at that moment: the '.$color[2].' carpet, the '.$color[1].' lawn, the '.$color[0].' house, the '.$color[5].' sky. The new president and his first lady. - Richard M. Nixon"</p>');
+    echo '<p>"The memory of that scene for me is like a frame of film forever frozen at that moment: the <b>'.$color[2].'</b> carpet, the <b>'.$color[1].'</b> lawn, the <b>'.$color[0].'</b> house, the <b>'.$color[5].'</b> sky. The new president and his first lady. - Richard M. Nixon"</p>';
 ?>
