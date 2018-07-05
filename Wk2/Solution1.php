f369767ead2ca9efe9a3eca4939254c0f3d4da0b
Tim Heilers <theile1083@my.sctd.edu>
2018-07-05 18:35:40 -0400

    Added Wk1 Homework Solutions

---
+++ Wk2/Solution1.php
@@ -0,0 +1,3 @@
+<?php
+    echo '<p>Tomorrow I \'ll learn PHP global variables.</p><p>This is a bad command : del c:\\*.*</p>';
+?>
---
+++ Wk2/Solution2.php
@@ -0,0 +1,4 @@
+<?php 
+    $color = array('white', 'green', 'red', 'blue', 'black', 'leaden');
+    echo('<p>"The memory of that scene for me is like a frame of film forever frozen at that moment: the '.$color[2].' carpet, the '.$color[1].' lawn, the '.$color[0].' house, the '.$color[5].' sky. The new president and his first lady. - Richard M. Nixon"</p>');
+?>
